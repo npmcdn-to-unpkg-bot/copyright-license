7f67500958e329d08edc2401c78e3c911991ba01
Andrew Suciu <andrewsuciu@gmail.com>
2015-06-04 14:57:38 -0500

    Beginning stripe interation.

--- copyright.py
+++ copyright.py
@@ -2,12 +2,23 @@ import os
 
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask.ext.sqlalchemy import SQLAlchemy
+
+import stripe
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = os.environ.get('DEBUG', False)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+# stripe_keys = {
+#     'secret_key': os.environ['SECRET_KEY'],
+#     'publishable_key': os.environ['PUBLISHABLE_KEY']
+# }
+#
+# stripe.api_key = stripe_keys['secret_key']
 
 db = SQLAlchemy(app)
 
@@ -16,9 +27,22 @@ def index():
     return render_template('index.jade')
 
 
-@app.route('/about/')
+@app.route('/about')
 def about():
     return render_template('about.jade')
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    pass
+
+@app.route('/purchase')
+def purchase():
+    return render_template('purchase.jade')
+
+@app.route('/create')
+def create():
+    return render_template('create.jade')
+
 
 
 @app.errorhandler(404)
