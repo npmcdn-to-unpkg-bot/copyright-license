545269338aab2ba5d2d4d203220ca80b3d580a47
Andrew Suciu <andrewsuciu@gmail.com>
2015-06-07 22:39:23 -0500

    Enable license registration.

--- copyright.py
+++ copyright.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+import requests
 
 from flask import Flask
 from flask import render_template
@@ -12,6 +14,10 @@ app = Flask(__name__)
 app.config['DEBUG'] = os.environ.get('DEBUG', False)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+app.config['SITE'] = 'https://connect.stripe.com'
+app.config['TOKEN_URI'] = '/oauth/token'
+app.config['CLIENT_ID'] = os.environ['CLIENT_ID']
+app.config['API_KEY'] = os.environ['API_KEY']
 
 # stripe_keys = {
 #     'secret_key': os.environ['SECRET_KEY'],
@@ -21,6 +27,34 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 # stripe.api_key = stripe_keys['secret_key']
 
 db = SQLAlchemy(app)
+
+
+class LicenseTerms(db.Model):
+    """
+    The terms under which an image can be licensed
+    """
+    __tablename__ = "terms"
+    id = db.Column(db.Integer, primary_key=True)
+    owner_stripe_token = db.Column(db.String())
+    amount = db.Column(db.String())
+    image_url = db.Column(db.String())
+    time_recorded = db.Column(db.DateTime())
+
+
+class License(db.Model):
+    """
+    A record of users who have paid for a license
+    """
+    __tablename__ = "licenses"
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String())
+    license_id = db.Column(db.Integer) #TODO make foreign key
+    time_recorded = db.Column(db.DateTime())
+
+# TODO: Enable charges
+# TODO: display licenses
+# TODO: Create records
+
 
 @app.route('/')
 def index():
@@ -41,8 +75,54 @@ def purchase():
 
 @app.route('/create')
 def create():
-    return render_template('create.jade')
+    return render_template('create.jade', token=None)
 
+
+@app.route('/register', methods=['POST'])
+def register_license():
+    url = request.form['url']
+    amount = request.form['amount']
+    token = request.form['token']
+
+    success = True
+    justification = ''
+
+    if LicenseTerms.query.filter_by(url=url).all():
+        success = False
+        justification = 'That image has already been registered'
+
+    new = LicenseTerms()
+    new.amount = amount
+    new.owner_stripe_token = token
+    new.image_url = url
+    new.time_recorded = datetime.datetime.now()
+
+    db.session.add(new)
+    db.session.commit()
+    return render_template('creation-outcome.jade', success=success, justification=justification)
+
+
+@app.route('/oauth/callback')
+def callback():
+    code = request.args.get('code')
+    data = {'grant_type': 'authorization_code',
+            'client_id': app.config['CLIENT_ID'],
+            'client_secret': app.config['API_KEY'],
+            'code': code}
+
+    # Make /oauth/token endpoint POST request
+    url = app.config['SITE'] + app.config['TOKEN_URI']
+    print("URL")
+    print(url)
+    resp = requests.post(url, params=data)
+    print("RESP")
+    print(resp)
+
+    # Grab access_token (use this as your user's API key)
+    resp = resp.json()
+    token = resp.get('access_token', None)
+    username = resp.get('stripe_user_id', "(Didn't get an ID from Stripe)")
+    return render_template('create.jade', token=token, username=username)
 
 
 @app.errorhandler(404)
