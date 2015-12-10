fbded6321feefa9d97a29e6b509a3cb5e282374b
Christopher Yeh <chrisyeh96@gmail.com>
2015-12-09 22:42:03 -0800

    Reset app.config variables to OS params (rather than test string)

--- copyright/config.py
+++ copyright/config.py
@@ -3,15 +3,15 @@ from copyright import app
 import os
 import stripe
 
-app.config['DEBUG'] = True # os.environ.get('DEBUG', False)
+app.config['DEBUG'] = os.environ.get('DEBUG', False)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.config['SITE'] = 'https://connect.stripe.com'
 app.config['TOKEN_URI'] = '/oauth/token'
-app.config['CLIENT_ID'] = 'test' # os.environ['CLIENT_ID']
+app.config['CLIENT_ID'] = os.environ['CLIENT_ID']
 
 stripe_keys = {
-    'secret_key': 'test', # os.environ['SECRET_KEY'],
-    'publishable_key': 'test' # os.environ['PUBLISHABLE_KEY']
+    'secret_key': os.environ['SECRET_KEY'],
+    'publishable_key': os.environ['PUBLISHABLE_KEY']
 }
 stripe.api_key = stripe_keys['secret_key']
