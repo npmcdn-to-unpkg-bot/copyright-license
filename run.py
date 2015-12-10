3968cb09e99d1298c839dc4ee27d830245f4248d
Christopher Yeh <chrisyeh96@gmail.com>
2015-12-09 22:32:03 -0800

    Try connecting to new port

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import os
+
 from copyright import app
 import copyright.config
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
