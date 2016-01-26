bafeda960df06a6a35485f5137705dc6cac7dd67
Hristo Stoyanov <stoyanov@stanford.edu>
2016-01-25 22:45:38 -0800

    Small change

--- copyright/config.py
+++ copyright/config.py
@@ -8,7 +8,7 @@ app.config['DEBUG'] = os.environ.get('DEBUG', False)
 app.config['SITE'] = 'https://connect.stripe.com'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['TOKEN_URI'] = '/oauth/token'
-app.config['UPLOAD_FOLDER'] = '/tmp'
+app.config['UPLOAD_FOLDER'] = '/tmp/copyright'
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
