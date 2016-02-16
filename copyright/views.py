d87a88656beea54ff8509ebce322ba0ad9b9b5e3
Christopher Yeh <chrisyeh96@gmail.com>
2016-02-15 22:01:28 -0800

    AWS Integration first try

--- copyright/views.py
+++ copyright/views.py
@@ -96,15 +96,42 @@ def create():
     return render_template('create.html', token=None)
 
 
+@app.route('/sign_s3/')
+def sign_s3():
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+    S3_BUCKET = os.environ.get('S3_BUCKET')
+
+    object_name = urllib.quote_plus(request.args.get('file_name'))
+    mime_type = request.args.get('file_type')
+
+    expires = int(time.time()+60*60*24)
+    amz_headers = "x-amz-acl:public-read"
+
+    string_to_sign = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, object_name)
+
+    signature = base64.encodestring(hmac.new(AWS_SECRET_KEY.encode(), string_to_sign.encode('utf8'), sha1).digest())
+    signature = urllib.quote_plus(signature.strip())
+
+    url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+
+    content = json.dumps({
+        'signed_request': '%s?AWSAccessKeyId=%s&Expires=%s&Signature=%s' % (url, AWS_ACCESS_KEY, expires, signature),
+        'url': url,
+    })
+    return content
+
+
 @app.route('/register', methods=['POST'])
 def register_license():
     amount = request.form['amount']
     stripe_user_id = request.form['id']
     description = request.form['description']
-    file = request.files['image']
-    r = redis.from_url(app.config['REDIS_URL'])
-    r.set(file.filename, file.read())
-    url = '/uploads/'+file.filename
+    # file = request.files['image']
+    # r = redis.from_url(app.config['REDIS_URL'])
+    # r.set(file.filename, file.read())
+    # url = '/uploads/'+file.filename
+    url = request.form['image_url']
     success = True
     justification = ''
 
